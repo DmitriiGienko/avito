c41ecb660721d40aafdb97510bc873d159f14606
Ilya <rabota003@yandex.ru>
2023-11-08 14:30:57 +0300

    Пробую делать тесты по комментариям

--- src/test/java/ru/skypro/homework/controller/CommentsControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/CommentsControllerTest.java
@@ -10,11 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import ru.skypro.homework.HomeworkApplication;
 import ru.skypro.homework.dto.CommentDTO;
 import ru.skypro.homework.mapper.CommentMapper;
 import ru.skypro.homework.model.CommentModel;
@@ -37,9 +39,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = HomeworkApplication.class)
 @Testcontainers
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class CommentsControllerTest {
     @Autowired
     MockMvc mockMvc;
